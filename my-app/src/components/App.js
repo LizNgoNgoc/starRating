0b905800528cf299f80e979d75c4b600aabefd33
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-01-10 17:43:24 +0300

    add test page

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+// import FaStars from './rating/FaStar';
+import FaStars from './rating/FaStar';
 
 function App() {
-  return (
-    <div className="App">
-      
+  return <div className="App">
+      <p>Hi</p>
+      <FaStars />
     </div>
-  );
 }
 
 export default App;
---
+++ my-app/src/components/rating/FaStar.jsx
@@ -0,0 +1,10 @@
+import styles from './fastar.module.css';
+
+function FaStars() {
+    return <div className={styles.container}>
+        <p className={styles.star}>Stars</p>
+    </div>
+   
+}
+
+export default FaStars;
