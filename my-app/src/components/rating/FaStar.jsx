e3ed702f73e8dfbfeb35e9f9cfd308a570c7b846
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-01-11 15:50:36 +0300

    add textarea tag

--- my-app/src/components/rating/FaStar.jsx
+++ my-app/src/components/rating/FaStar.jsx
@@ -3,6 +3,7 @@ import styles from './fastar.module.css';
 function FaStars() {
     return <div className={styles.container}>
         <p className={styles.star}>Stars</p>
+        <textarea className={styles.textarea}></textarea>
     </div>
    
 }
