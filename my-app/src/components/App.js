697847a80da39a7a3c5138043538f6cf1af30393
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-01-11 17:58:16 +0300

    update name of components

--- my-app/src/components/App.js
+++ my-app/src/components/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 // import FaStars from './rating/FaStar';
-import FaStars from './rating/FaStar';
+import FaStars from './rating/FaStars';
 
 function App() {
   return <div className="App">
--- my-app/src/components/rating/FaStars.jsx
+++ my-app/src/components/rating/FaStars.jsx
@@ -1,8 +1,10 @@
-import styles from './fastar.module.css';
+import styles from './fastars.module.css';
+import Stars from '../stars/Stars';
 
 function FaStars() {
     return <div className={styles.container}>
         <p className={styles.star}>Stars</p>
+        <Stars />
         <textarea className={styles.textarea}></textarea>
     </div>
    
