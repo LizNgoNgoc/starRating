b254dc5cc25b650a80a30213445dc38efb317bd5
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-01-11 17:56:15 +0300

    update Stars plus react-icons

--- my-app/src/components/stars/Stars.jsx
+++ my-app/src/components/stars/Stars.jsx
@@ -1,7 +1,20 @@
-import styles from './stars.module.css'
+import styles from './stars.module.css';
+import { FaStar } from 'react-icons/fa';
 
 function Stars() {
+    const stars = Array(5).fill(0)
+
     return <>
+    <div className={styles.star}>
+        {stars.map((_, index) => {
+            return(
+                <FaStar 
+                    key={index}
+                />
+            )
+        })}
+    </div>
+    
     </>
 }
 
