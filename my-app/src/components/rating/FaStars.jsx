9466ed67ab5411d235fcd55d8fe486fffcacc6af
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-01-12 14:50:26 +0300

    update component Form, import this component

--- my-app/src/components/rating/FaStars.jsx
+++ my-app/src/components/rating/FaStars.jsx
@@ -1,11 +1,12 @@
 import styles from './fastars.module.css';
 import Stars from '../stars/Stars';
+import Form from '../review/Form';
 
 function FaStars() {
     return <div className={styles.container}>
         <p className={styles.star}>Stars</p>
         <Stars />
-        <textarea className={styles.textarea}></textarea>
+        <Form />
     </div>
    
 }
--- my-app/src/components/review/Form.jsx
+++ my-app/src/components/review/Form.jsx
@@ -3,7 +3,9 @@ import styles from './form.module.css';
 
 function Form() {
     return <form>
-
+        <input type='text' name='name' placeholder='Name'></input>
+        <input type='text' name='email' placeholder='Email'></input>
+        <textarea placeholder='Your review' className={styles.textarea}></textarea>
     </form>
 }
 
