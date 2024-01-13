af4841cb1cc69db62d8c4b241aba761904d60e4f
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-01-13 16:12:20 +0300

    add hooks useState

--- my-app/src/components/stars/Stars.jsx
+++ my-app/src/components/stars/Stars.jsx
@@ -1,14 +1,23 @@
+import { useState } from 'react';
 import styles from './stars.module.css';
 import { FaStar } from 'react-icons/fa';
 
+const colors = {
+    orange: '#FFBA5A',
+    grey: '#a9a9a9'
+}
+
 function Stars() {
     const stars = Array(5).fill(0)
+    const [currentValue, setCurrentValue] = useState(0);
+    const [hoverValue, setHoverValue] = useState(undefined);
+
 
     return <>
     <div className={styles.star}>
         {stars.map((_, index) => {
             return(
-                <FaStar key={index} className={styles.star_img}/>
+                <FaStar key={index} size={30} className={styles.img_stars}/>
             )
         })}
     </div>
