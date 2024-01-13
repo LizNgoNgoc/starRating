622757e3c4acf7a376f0d2bf821b386bfbc798f9
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-01-13 16:02:09 +0300

    update form css

--- my-app/src/components/review/Form.jsx
+++ my-app/src/components/review/Form.jsx
@@ -2,9 +2,9 @@ import styles from './form.module.css';
 
 
 function Form() {
-    return <form>
-        <input type='text' name='name' placeholder='Name'></input>
-        <input type='text' name='email' placeholder='Email'></input>
+    return <form className={styles.form}>
+        <input type='text' name='name' placeholder='Name' className={styles.input}></input>
+        <input type='text' name='email' placeholder='Email' className={styles.input}></input>
         <textarea placeholder='Your review' className={styles.textarea}></textarea>
     </form>
 }
