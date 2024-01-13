df4fa475a235fc3c9cbd8d8e294f702f7214a38c
Liza Ngo <83579019+LizNgoNgoc@users.noreply.github.com>
2024-01-13 16:38:41 +0300

    add function onClick change stars color

--- my-app/src/components/stars/Stars.jsx
+++ my-app/src/components/stars/Stars.jsx
@@ -2,22 +2,38 @@ import { useState } from 'react';
 import styles from './stars.module.css';
 import { FaStar } from 'react-icons/fa';
 
-const colors = {
-    orange: '#FFBA5A',
-    grey: '#a9a9a9'
-}
+
 
 function Stars() {
+    const colors = {
+        orange: '#FFBA5A',
+        grey: '#a9a9a9'
+    }
     const stars = Array(5).fill(0)
-    const [currentValue, setCurrentValue] = useState(0);
-    const [hoverValue, setHoverValue] = useState(undefined);
+    const [currentValue, setCurrentValue] = useState(0); //хук для подсветки звезд при клике
+    const [hoverValue, setHoverValue] = useState(undefined); //хук для подсветки звезд при наведении мыши
+
+    const handleClick = value => {
+        return setCurrentValue(value)
+    }
+
+    function handleMouseOver(value) {
+        return setHoverValue(value)
+    }
+
+    function handleMouseLeave() {
+        return setHoverValue(undefined)
+    }
 
 
     return <>
     <div className={styles.star}>
         {stars.map((_, index) => {
             return(
-                <FaStar key={index} size={30} className={styles.img_stars}/>
+                <FaStar key={index} size={30} className={styles.img_stars} 
+                    color={(hoverValue || currentValue) > index ? colors.orange : colors.grey}
+                    onClick={() => handleClick(index + 1)}
+                />
             )
         })}
     </div>
